971c373bdb6da04e220dcbf9258e7b617d6acaf4
N3232 <N3232@student.jamk.fi>
2021-12-13 12:37:08 +0200

    shortened code

--- effects.h
+++ effects.h
@@ -27,10 +27,6 @@ class Effects
             {
                 matrix[x][y] = 1;
                 matrix[x][y-1] = 0;
-                if(y == 0)
-                {
-                    matrix[x][-1] = 0;
-                }
             }
         }
     }
@@ -43,16 +39,7 @@ class Effects
             for(int y = 0; y < matrix[0].size(); y++)
             {
                 matrix[x][y] = 1;
-                if(x > 0)
-                {
-                    matrix[x-1][y] = 0;
-                }
-                if(x == 0)
-                {
-                    //matrix[0].back or matrix[0][-1]? I can't decide
-                    //Benchmark, anyone?
-                    matrix[x].back() = 0;
-                }
+                if(x > 0) matrix[x-1][y] = 0;
             }
         }
     }
@@ -77,7 +64,7 @@ class Effects
         std::this_thread::sleep_for(std::chrono::milliseconds(60));
         if(OS == "linux") system("clear");
         else if(OS == "windows") system("cls");
-        
+
         //Last numbers in row and in line will be always 1.
         //That's why they need to be clipped out with matrix.size()-1
         for(int x = 0; x < matrix.size()-1; x++)
